1dff1a0b8e7ab4b826e104496de8289466847319
dare-devil-ex <kmsedition@gmail.com>
2025-01-22 23:59:18 +0530

    modified:   garage/links.txt 	modified:   userFetch.py

--- userFetch.py
+++ userFetch.py
@@ -1,3 +1,45 @@
+from requests import get, exceptions
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+uName = input("Enter the username: ")
+valid = 0
+invalid = 0
+
+def wkaie(url):
+    global valid, invalid
+    try:
+        resp = get(url, timeout=10).status_code
+        if resp == 200:
+            valid += 1
+            print(f"{valid}: {url}")
+            with open(f"{uName}.txt", "a+") as wkaie:
+                wkaie.write(f"{url}\n")
+        else:
+            invalid += 1
+    except exceptions.Timeout:
+        pass
+    except Exception as e:
+        pass
+
+urls = []
 with open("./garage/links.txt", "r") as f:
-    for wkaie in f.readlines():
-        print(wkaie.strip())
+    with open("./garage/links.txt", "r") as f:
+        for line in f:
+            line = line.strip()
+            if "github.com" in line:
+                url = line.replace("$username", uName.replace("_", "-"))
+            elif "facebook.com" in line:
+                url = line.replace("$username", uName.replace("_", "."))
+            elif "instagram.com" in line:
+                url = line.replace("$username", uName.replace("_", ""))
+            else:
+                url = line.replace("$username", uName)
+            urls.append(url)
+
+with ThreadPoolExecutor(max_workers=10) as executor:
+    futures = [executor.submit(wkaie, url) for url in urls]
+    for future in as_completed(futures):
+        pass
+
+print("Scanning completed")
+print(f"{valid} valid URLs found!\n{invalid} invalid URLs found.")
